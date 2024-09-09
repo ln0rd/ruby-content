33cf4b62c40f32cdcbbe83cba7f9b396dbbd5acd
leonrd_lbf <leonrd.lbf@gmail.com>
2024-09-09 19:40:02 -0300

    feat: primitive types and print

---
+++ examples/primite-types.rb
@@ -0,0 +1,21 @@
+
+color = "red"
+name = "ln0rd"
+number = 23
+othernumber = 21.5231231
+itsBigger = 1 > 0
+
+CONSTANT = "constant value string"
+
+puts number.class # Integer
+puts color.class # String
+puts othernumber.class # Float
+puts number.class # Integer
+puts name.class # String
+puts itsBigger.class # TrueClass
+
+
+
+
+
+
---
+++ examples/print.rb
@@ -0,0 +1,7 @@
+puts 'Hello'
+
+puts "Hi"
+
+puts 2 + 3
+
+puts "soma #{1 + 2}"
